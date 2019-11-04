98c2542929659b566d1da8f20e0302c0ae29cb98
Anurag Mishra <40445789+anuragsurfing123@users.noreply.github.com>
2019-11-04 21:18:31 +0530

    first commit

---
+++ angularservucereq.js
@@ -0,0 +1,47 @@
+var app = angular.module("module1",[]).controller("controller1",function($scope)
+{
+  $scope.count = 0;
+  $scope.count2 = 0;
+  $scope.count3 = 0;
+  $scope.count4 = 0;
+  $scope.count5 = 0;
+  $scope.count6 = 0;
+  $scope.count7 = 0;
+  $scope.count8 = 0;
+  $scope.count9 = 0;
+  $scope.count10 = 0;
+  $scope.count11= 0;
+  $scope.count12= 0;
+  $scope.count13= 0;
+  $scope.count14= 0;
+  $scope.count15= 0;
+  $scope.count16= 0;
+  $scope.count17= 0;
+  $scope.count18= 0;
+  $scope.count19= 0;
+  $scope.count20= 0;
+  $scope.count21= 0;
+  $scope.count22= 0;
+  $scope.count23= 0;
+  $scope.count24= 0;
+  $scope.count25= 0;
+  $scope.count26= 0;
+  $scope.count27= 0;
+  $scope.count28= 0;
+  $scope.count29= 0;
+  $scope.count30 = 0;
+  $scope.count31= 0;
+  $scope.count32= 0;
+  $scope.count33= 0;
+  $scope.count34= 0;
+  $scope.count35= 0;
+  $scope.count36= 0;
+  $scope.count37= 0;
+  $scope.count38= 0;
+  $scope.count39= 0;
+  
+
+}
+
+);
+
